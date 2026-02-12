bceb7a676a20670433e4809750e1ad3c77a61250
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2026-02-12 19:31:49 +0700

    refactor: separate TestSeeder from UserSeeder

--- Master/Seeds/TestSeeder.php
+++ Master/Seeds/TestSeeder.php
@@ -4,13 +4,13 @@ namespace Teguh02\Rijanphp\Master\Seeds;
 
 use Teguh02\Rijanphp\Core\Database\Seeder\Seeder;
 
-class UserSeeder extends Seeder
+class TestSeeder extends Seeder
 {
     public function run()
     {
         db()->table('users')->insert([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
+            'name' => 'Test User',
+            'email' => 'test@example.com',
             'password' => password_hash('password', PASSWORD_BCRYPT),
         ]);
     }
