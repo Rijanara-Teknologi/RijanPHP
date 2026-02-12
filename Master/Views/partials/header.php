668560e0adb7e571b85574b1ac20aaa849dbb85d
GitHub Action <action@github.com>
2026-02-12 16:00:38 +0000

    chore: cleanup non-core files for stable branch

--- Master/Views/partials/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <h1>Master Header Component</h1>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/products">Products</a></li>
-        </ul>
-    </nav>
-</header>
